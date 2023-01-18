7ee6365dc2ffb42116d75a7083f907c3aa2fcf92
eun <bootcamp0eun@gmail.com>
2023-01-18 16:34:33 +0900

    9.1

--- day05 practice.py
+++ day05 practice.py
@@ -1,25 +1,7 @@
-# 8.11
-print("# 8.11")
-odd_set = {i for i in range(10) if i % 2 != 0}
-print(odd_set)
+# 9.1
+print("# 9.1")
+def good():
+    return ['Harry', 'Ron', 'Hermione']
 
-# 8.12
-print("\n# 8.12")
-for i in (n for n in range(10)):
-    print(f"Got {i}")
 
-# 8.13
-print("\n# 8.13")
-keys = ('optimist', 'pessimist', 'troll')
-values = ('The glass is half full', 'The glass is half empty', 'How did you get a glass?')
-new = {k: v for k, v in zip(keys, values)}
-new2 = dict(zip(keys, values))
-print(new)
-print(new2)
-
-# 8.14
-print("\n# 8.14")
-titles = ['Creature of Habit', 'Crewel Fate']
-plots = ["A nun turns into a mon ster", "A haunted yarn shop"]
-movies = dict(zip(titles, plots))
-print(movies)
+print(good())
