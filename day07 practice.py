f00b50f76c615da6d6629dc0d1a444b69c17e6e8
eun <bootcamp0eun@gmail.com>
2023-01-20 16:45:45 +0900

    10.7

--- day07 practice.py
+++ day07 practice.py
@@ -1,14 +1,14 @@
-# 10.6
-print('# 10.6')
+# 10.7
+print('# 10.7')
 class Element:
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
 
-    def dump(self):
-        print(f'{self.name}, {self.symbol}, {self.number}')
+    def __str__(self):
+        return f'{self.name}, {self.symbol}, {self.number}'
 
 
 hydrogen = Element('Hydrogen', 'H', 1)
-hydrogen.dump()
+print(hydrogen)
