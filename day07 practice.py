a5a694aa990b9cea1bb7f10567fadde452bfc12e
eun <bootcamp0eun@gmail.com>
2023-01-20 17:10:05 +0900

    10.8 but i cannot understand

--- day07 practice.py
+++ day07 practice.py
@@ -1,14 +1,24 @@
-# 10.7
-print('# 10.7')
+# 10.8
+print('# 10.8')
 class Element:
     def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
 
-    def __str__(self):
-        return f'{self.name}, {self.symbol}, {self.number}'
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    @property
+    def number(self):
+        return self.__number
 
 
 hydrogen = Element('Hydrogen', 'H', 1)
 print(hydrogen)
+print(hydrogen.name, hydrogen.symbol, hydrogen.number)
