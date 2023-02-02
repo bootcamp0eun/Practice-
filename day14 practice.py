882e7bf21e49f5928e6b8c85e7608b37bfbc86ff
eun <bootcamp0eun@gmail.com>
2023-02-02 16:26:21 +0900

    10.6

--- day14 practice.py
+++ day14 practice.py
@@ -1,44 +1,16 @@
-# 10.1
-class Thing:
-    pass
-
-
-print(Thing)
-example = Thing()
-print(example)
-
-
-# 10.2
-class Thing2:
-    letters = 'abc'
-
-
-print(Thing2.letters)
-
-
-# 10.3
-class Thing3:
-    def __init__(self):
-        self.letters = 'xyz'
-
-
-home = Thing3()
-print(home.letters)
-
-
-# 10.4
+# 10.6
 class Element:
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
 
+    def dump(self):
+        print(f"name={self.name}\nsymbol={self.symbol}\nnumber={self.number}")
 
-make = Element("Hydrogen", "H", 1)
 
-
-# 10.5
 el_dict = {"name": "Hydrogen",
            "symbol": "H",
            "number": 1}
 hydrogen = Element(el_dict["name"], el_dict["symbol"], el_dict["number"])
+hydrogen.dump()
