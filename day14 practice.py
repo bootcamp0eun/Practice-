cc3d012e443b455694f45f333653568fb5236b2d
eun <bootcamp0eun@gmail.com>
2023-02-02 16:27:37 +0900

    10.7

--- day14 practice.py
+++ day14 practice.py
@@ -1,16 +1,16 @@
-# 10.6
+# 10.7
 class Element:
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
 
-    def dump(self):
-        print(f"name={self.name}\nsymbol={self.symbol}\nnumber={self.number}")
+    def __str__(self):
+        return f"name={self.name}\nsymbol={self.symbol}\nnumber={self.number}"
 
 
 el_dict = {"name": "Hydrogen",
            "symbol": "H",
            "number": 1}
 hydrogen = Element(el_dict["name"], el_dict["symbol"], el_dict["number"])
-hydrogen.dump()
+print(hydrogen)
