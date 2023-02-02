8fe56a1d4678d432410dfbc172dfbcbb087da951
eun <bootcamp0eun@gmail.com>
2023-02-02 16:36:44 +0900

    10.9

--- day14 practice.py
+++ day14 practice.py
@@ -1,24 +1,24 @@
-# 10.8
-class Element:
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
-
-    @property
-    def get_name(self):
-        return self.name
-
-    @property
-    def get_symbol(self):
-        return self.symbol
-
-    @property
-    def get_number(self):
-        return self.number
+# 10.9
+class Bear:
+    def eats(self):
+        return "berries"
 
 
-el_dict = {"name": "Hydrogen",
-           "symbol": "H",
-           "number": 1}
-hydrogen = Element(el_dict["name"], el_dict["symbol"], el_dict["number"])
+class Rabbit:
+    def eats(self):
+        return "clover"
+
+
+class Octothorpe:
+    def eats(self):
+        return "campers"
+
+
+b = Bear()
+r = Rabbit()
+o = Octothorpe()
+
+print(f"{b.eats()},  {r.eats()},  {o.eats()}")
+
+
+
